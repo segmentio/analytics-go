5a3f67c0013bc6bae889f04b0fe3d8b4f0d839d3
TJ Holowaychuk <tj@vision-media.ca>
2014-08-26 16:35:41 -0700

    re-fix context.library

--- analytics.go
+++ analytics.go
@@ -166,8 +166,10 @@ func newMessage(call string) Message {
 		"timestamp": timestamp(),
 		"messageId": uid(),
 		"context": map[string]interface{}{
-			"version": Version,
-			"library": "analytics-go",
+			"library": map[string]interface{}{
+				"name":    "analytics-go",
+				"version": Version,
+			},
 		},
 	}
 }
