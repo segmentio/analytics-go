0d0bd4ffec610c45e70d17b6408807df0c21e1e3
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2015-01-27 23:47:19 +0700

    Refactoring all tests
    - add global variable 'client' and use it everywhere
    - add TestCases structure to representing cases in tests
    - add LoopAssertion method

--- analytics_test.go
+++ analytics_test.go
@@ -6,6 +6,16 @@ import (
 	"time"
 )
 
+var (
+	client = New("SECRET")
+)
+
+// TestCases represent case on test
+type TestCases struct {
+	msg          Message
+	expected_err error
+}
+
 // New should return a *Client with default value
 func TestNew(t *testing.T) {
 	cases := []struct {
@@ -42,190 +52,95 @@ func TestNew(t *testing.T) {
 
 // Identify should return an expected error if "userId" or "anonymousId" doesn't exists
 func TestIdentify(t *testing.T) {
-	valid_client := New("this is a secret, you can't read it. don't you?")
-	cases := []struct {
-		client       *Client
-		msg          Message
-		expected_err error
-	}{
-		{
-			valid_client,
-			Message{"userId": "1", "traits": map[string]interface{}{}},
-			nil,
-		},
-		{
-			valid_client,
-			Message{"anonymousId": "1", "traits": map[string]interface{}{}},
-			nil,
-		},
-		{
-			valid_client,
-			Message{"traits": map[string]interface{}{}},
-			errors.New("You must pass either an 'anonymousId' or 'userId'."),
-		},
+	cases := []TestCases{
+		{Message{"userId": "1", "traits": map[string]interface{}{}}, nil},
+		{Message{"anonymousId": "1", "traits": map[string]interface{}{}}, nil},
+		{Message{"traits": map[string]interface{}{}}, errors.New("You must pass either an 'anonymousId' or 'userId'.")},
 	}
 
-	for _, c := range cases {
-		err := valid_client.Identify(c.msg)
-		if (err == nil && c.expected_err != nil) || (err != nil && c.expected_err == nil) {
-			t.Errorf("got: %v, expected: %v", err, c.expected_err)
-		}
-		if err != nil && c.expected_err != nil {
-			if err.Error() != c.expected_err.Error() {
-				t.Errorf("got: %v, expected: %v", err, c.expected_err)
-			}
-		}
-
-	}
+	LoopAssertion(t, cases, client.Identify)
 }
 
 // Track should return an expected error if the following are true:
 // 1. "event" doesn't exists
 // 2. "userId" or "anonymousId" doesn't exists
 func TestTrack(t *testing.T) {
-	valid_client := New("this is an another secret. please don't read it.")
-	cases := []struct {
-		client       *Client
-		msg          Message
-		expected_err error
-	}{
+	cases := []TestCases{
 		{
-			valid_client,
 			Message{"userId": "1", "event": "registered",
 				"properties": map[string]interface{}{}},
 			nil,
 		},
 		{
-			valid_client,
 			Message{"anonymousId": "1", "event": "registered",
 				"properties": map[string]interface{}{}},
 			nil,
 		},
 		{
-			valid_client,
 			Message{"event": "registered", "properties": map[string]interface{}{}},
 			errors.New("You must pass either an 'anonymousId' or 'userId'."),
 		},
 		{
-			valid_client,
 			Message{"userId": "1", "properties": map[string]interface{}{}},
 			errors.New("You must pass 'event'."),
 		},
 		{
-			valid_client,
 			Message{"anonymousId": "1", "properties": map[string]interface{}{}},
 			errors.New("You must pass 'event'."),
 		},
 	}
 
-	for _, c := range cases {
-		err := valid_client.Track(c.msg)
-		if (err == nil && c.expected_err != nil) || (err != nil && c.expected_err == nil) {
-			t.Errorf("got: %v, expected: %v", err, c.expected_err)
-		}
-		if err != nil && c.expected_err != nil {
-			if err.Error() != c.expected_err.Error() {
-				t.Errorf("got: %v, expected: %v", err, c.expected_err)
-			}
-		}
-
-	}
+	LoopAssertion(t, cases, client.Track)
 }
 
 // Page should return an expected error if userId" or "anonymousId" doesn't exists
 func TestPage(t *testing.T) {
-	valid_client := New("i warn you to not read this. but ... you ... argh ...")
-	cases := []struct {
-		client       *Client
-		msg          Message
-		expected_err error
-	}{
+	cases := []TestCases{
 		{
-			valid_client,
 			Message{"userId": "1", "name": "About", "category": "Help",
 				"properties": map[string]interface{}{}},
 			nil,
 		},
 		{
-			valid_client,
 			Message{"anonymousId": "1", "name": "About", "category": "Help",
 				"properties": map[string]interface{}{}},
 			nil,
 		},
 		{
-			valid_client,
 			Message{"name": "Home", "properties": map[string]interface{}{}},
 			errors.New("You must pass either an 'anonymousId' or 'userId'."),
 		},
 	}
-
-	for _, c := range cases {
-		err := valid_client.Page(c.msg)
-		if (err == nil && c.expected_err != nil) || (err != nil && c.expected_err == nil) {
-			t.Errorf("got: %v, expected: %v", err, c.expected_err)
-		}
-		if err != nil && c.expected_err != nil {
-			if err.Error() != c.expected_err.Error() {
-				t.Errorf("got: %v, expected: %v", err, c.expected_err)
-			}
-		}
-
-	}
+	LoopAssertion(t, cases, client.Page)
 }
 
 // Screen should return an expected error if userId" or "anonymousId" doesn't exists
 func TestScreen(t *testing.T) {
-	valid_client := New("i'm out")
-	cases := []struct {
-		client       *Client
-		msg          Message
-		expected_err error
-	}{
+	cases := []TestCases{
 		{
-			valid_client,
 			Message{"userId": "1", "name": "Cover", "category": "Guide",
 				"properties": map[string]interface{}{}},
 			nil,
 		},
 		{
-			valid_client,
 			Message{"anonymousId": "1", "name": "About", "category": "About",
 				"properties": map[string]interface{}{}},
 			nil,
 		},
 		{
-			valid_client,
 			Message{"name": "Post New", "properties": map[string]interface{}{}},
 			errors.New("You must pass either an 'anonymousId' or 'userId'."),
 		},
 	}
-
-	for _, c := range cases {
-		err := valid_client.Screen(c.msg)
-		if (err == nil && c.expected_err != nil) || (err != nil && c.expected_err == nil) {
-			t.Errorf("got: %v, expected: %v", err, c.expected_err)
-		}
-		if err != nil && c.expected_err != nil {
-			if err.Error() != c.expected_err.Error() {
-				t.Errorf("got: %v, expected: %v", err, c.expected_err)
-			}
-		}
-
-	}
+	LoopAssertion(t, cases, client.Screen)
 }
 
 // Group should return an expected error if the following are true:
 // 1. "groupId" doesn't exists
 // 2. "userId" or "anonymousId" doesn't exists
 func TestGroup(t *testing.T) {
-	valid_client := New("OH: this is a secret boy!")
-	cases := []struct {
-		client       *Client
-		msg          Message
-		expected_err error
-	}{
+	cases := []TestCases{
 		{
-			valid_client,
 			Message{
 				"userId":  "1",
 				"groupId": "1",
@@ -234,7 +149,6 @@ func TestGroup(t *testing.T) {
 			nil,
 		},
 		{
-			valid_client,
 			Message{
 				"anonymousId": "1",
 				"groupId":     "1",
@@ -243,7 +157,6 @@ func TestGroup(t *testing.T) {
 			nil,
 		},
 		{
-			valid_client,
 			Message{
 				"groupId": "1",
 				"traits":  map[string]interface{}{},
@@ -251,7 +164,6 @@ func TestGroup(t *testing.T) {
 			errors.New("You must pass either an 'anonymousId' or 'userId'."),
 		},
 		{
-			valid_client,
 			Message{
 				"userId": "1",
 				"traits": map[string]interface{}{},
@@ -259,7 +171,6 @@ func TestGroup(t *testing.T) {
 			errors.New("You must pass a 'groupId'."),
 		},
 		{
-			valid_client,
 			Message{
 				"anonymousId": "1",
 				"traits":      map[string]interface{}{},
@@ -267,53 +178,24 @@ func TestGroup(t *testing.T) {
 			errors.New("You must pass a 'groupId'."),
 		},
 	}
-
-	for _, c := range cases {
-		err := valid_client.Group(c.msg)
-		if (err == nil && c.expected_err != nil) || (err != nil && c.expected_err == nil) {
-			t.Errorf("got: %v, expected: %v", err, c.expected_err)
-		}
-		if err != nil && c.expected_err != nil {
-			if err.Error() != c.expected_err.Error() {
-				t.Errorf("got: %v, expected: %v", err, c.expected_err)
-			}
-		}
-
-	}
+	LoopAssertion(t, cases, client.Group)
 }
 
 // Alias should return an expected error if "userId" or "previousId" doesn't exists
 func TestAlias(t *testing.T) {
-	valid_client := New("Sugar ... Yes Please .. (Maroon 5 Sugar)")
-	cases := []struct {
-		client       *Client
-		msg          Message
-		expected_err error
-	}{
-		{
-			valid_client,
-			Message{"userId": "1", "previousId": "1"},
-			nil,
-		},
-		{
-			valid_client,
-			Message{"userId": "1"},
-			errors.New("You must pass a 'previousId'."),
-		},
-		{
-			valid_client,
-			Message{"previousId": "1"},
-			errors.New("You must pass a 'userId'."),
-		},
-		{
-			valid_client,
-			Message{"someId": "1", "otherId": "1"},
-			errors.New("You must pass a 'userId'."),
-		},
+	cases := []TestCases{
+		{Message{"userId": "1", "previousId": "1"}, nil},
+		{Message{"userId": "1"}, errors.New("You must pass a 'previousId'.")},
+		{Message{"previousId": "1"}, errors.New("You must pass a 'userId'.")},
+		{Message{"someId": "1", "otherId": "1"}, errors.New("You must pass a 'userId'.")},
 	}
 
+	LoopAssertion(t, cases, client.Alias)
+}
+
+func LoopAssertion(t *testing.T, cases []TestCases, exported func(msg Message) error) {
 	for _, c := range cases {
-		err := valid_client.Alias(c.msg)
+		err := exported(c.msg)
 		if (err == nil && c.expected_err != nil) || (err != nil && c.expected_err == nil) {
 			t.Errorf("got: %v, expected: %v", err, c.expected_err)
 		}
@@ -322,6 +204,5 @@ func TestAlias(t *testing.T) {
 				t.Errorf("got: %v, expected: %v", err, c.expected_err)
 			}
 		}
-
 	}
 }
