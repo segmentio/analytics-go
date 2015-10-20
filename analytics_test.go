94ea05444ef8e39c99bb45265d754c91f9af6c5f
Prateek Srivastava <f2prateek@gmail.com>
2015-10-19 17:46:14 -0700

    Add test to verify library flushes at intervals

--- analytics_test.go
+++ analytics_test.go
@@ -87,6 +87,54 @@ func ExampleTrack() {
 	// }
 }
 
+func ExampleInterval() {
+	body, server := mockServer()
+	defer server.Close()
+
+	client := New("h97jamjwbh")
+	client.Endpoint = server.URL
+	client.now = mockTime
+	client.uid = mockId
+
+	client.Track(&Track{
+		Event:  "Download",
+		UserId: "123456",
+		Properties: map[string]interface{}{
+			"application": "Segment Desktop",
+			"version":     "1.1.0",
+			"platform":    "osx",
+		},
+	})
+
+	// Will flush in 5 seconds (default interval).
+	fmt.Printf("%s\n", <-body)
+	// Output:
+	// {
+	//   "batch": [
+	//     {
+	//       "event": "Download",
+	//       "messageId": "I'm unique",
+	//       "properties": {
+	//         "application": "Segment Desktop",
+	//         "platform": "osx",
+	//         "version": "1.1.0"
+	//       },
+	//       "timestamp": "2009-11-10T23:00:00+0000",
+	//       "type": "track",
+	//       "userId": "123456"
+	//     }
+	//   ],
+	//   "context": {
+	//     "library": {
+	//       "name": "analytics-go",
+	//       "version": "2.0.0"
+	//     }
+	//   },
+	//   "messageId": "I'm unique",
+	//   "sentAt": "2009-11-10T23:00:00+0000"
+	// }
+}
+
 func ExampleTrackWithTimestampSet() {
 	body, server := mockServer()
 	defer server.Close()
