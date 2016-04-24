d26d08cb40390b99c45a08666a53ddac2e800371
Achille Roussel <achille@segment.com>
2016-04-23 20:53:36 -0700

    add more unit tests

--- context_test.go
+++ context_test.go
@@ -26,7 +26,22 @@ func TestMakeJsonContextNonNil(t *testing.T) {
 	}
 }
 
-func TestContextMarshalJSON(t *testing.T) {
+func TestContextMarshalJSONLibrary(t *testing.T) {
+	c := Context{
+		Library: LibraryInfo{
+			Name: "testing",
+		},
+	}
+
+	if b, err := json.Marshal(c); err != nil {
+		t.Error("marshaling context object failed:", err)
+
+	} else if s := string(b); s != `{"library":{"name":"testing"}}` {
+		t.Error("invalid marshaled representation of context:", s)
+	}
+}
+
+func TestContextMarshalJSONExtra(t *testing.T) {
 	c := Context{
 		Extra: map[string]interface{}{
 			"answer": 42,
