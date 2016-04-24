5700178a92ec7ba516c3f7b193501c062fe8d0a0
Achille Roussel <achille@segment.com>
2016-04-23 23:09:52 -0700

    add Config

--- analytics.go
+++ analytics.go
@@ -3,22 +3,15 @@ package analytics
 import (
 	"io"
 	"io/ioutil"
-	"sync"
 
 	"bytes"
 	"encoding/json"
 	"net/http"
 	"time"
-
-	"github.com/segmentio/backo-go"
-	"github.com/xtgo/uuid"
 )
 
 // Version of the client.
 const Version = "3.0.0"
-
-// Endpoint for the Segment API.
-const Endpoint = "https://api.segment.io"
 
 // DefaultContext of message batches.
 var DefaultContext = map[string]interface{}{
@@ -28,65 +21,75 @@ var DefaultContext = map[string]interface{}{
 	},
 }
 
-// Backoff policy.
-var Backo = backo.DefaultBacko()
+type Client interface {
+	io.Closer
 
-// Client which batches messages and flushes at the given Interval or
-// when the Size limit is exceeded. Set Verbose to true to enable
-// logging output.
-type Client struct {
-	Endpoint string
-	// Interval represents the duration at which messages are flushed. It may be
-	// configured only before any messages are enqueued.
-	Interval  time.Duration
-	Size      int
-	Logger    Logger
-	Verbose   bool
-	Transport http.RoundTripper
-	key       string
-	msgs      chan interface{}
-	quit      chan struct{}
-	shutdown  chan struct{}
-	uid       func() string
-	now       func() time.Time
-	once      sync.Once
+	Enqueue(Message) error
 }
 
-// New client with write key.
-func New(key string) *Client {
-	c := &Client{
-		Endpoint:  Endpoint,
-		Interval:  5 * time.Second,
-		Size:      250,
-		Logger:    newDefaultLogger(),
-		Verbose:   false,
-		Transport: http.DefaultTransport,
-		key:       key,
-		msgs:      make(chan interface{}, 100),
-		quit:      make(chan struct{}),
-		shutdown:  make(chan struct{}),
-		now:       time.Now,
-		uid:       uid,
-	}
+type client struct {
+	Config
+	key string
 
+	// This channel is where the `Enqueue` method writes messages so they can be
+	// picked up and pushed by the backend goroutine taking care of applyinh the
+	// batching rules.
+	msgs chan interface{}
+
+	// These two channels are used to synchronize the client shutting down when
+	// `Close` is called.
+	// The first channel is closed to signal the backend goroutine that it has
+	// to stop, then the second one is closed by the backend goroutine to signal
+	// that it has finished flushing all queued messages.
+	quit     chan struct{}
+	shutdown chan struct{}
+}
+
+func New(writeKey string) Client {
+	c, _ := NewWithConfig(writeKey, Config{})
 	return c
 }
 
-func (c *Client) Enqueue(msg Message) (err error) {
-	if err = msg.validate(); err != nil {
+func NewWithConfig(writeKey string, config Config) (cli Client, err error) {
+	if err = config.validate(); err != nil {
 		return
 	}
-	c.once.Do(c.startLoop)
-	c.msgs <- msg.serializable(c.uid(), c.now())
+
+	c := &client{
+		Config:   makeConfig(config),
+		key:      writeKey,
+		msgs:     make(chan interface{}, 100),
+		quit:     make(chan struct{}),
+		shutdown: make(chan struct{}),
+	}
+
+	go c.loop()
+
+	cli = c
 	return
 }
 
-func (c *Client) startLoop() {
-	go c.loop()
+func (c *client) Enqueue(msg Message) (err error) {
+	if err = msg.validate(); err != nil {
+		return
+	}
+
+	defer func() {
+		// When the `msgs` channel is closed writing to it will trigger a panic.
+		// To avoid letting the panic propagate to the caller we recover from it
+		// and instead report that the client has been closed and shouldn't be
+		// used anymore.
+		if recover() != nil {
+			err = io.EOF
+		}
+	}()
+
+	c.msgs <- msg.serializable(c.UID(), c.Now())
+	return
 }
 
 // Close and flush metrics.
-func (c *Client) Close() (err error) {
+func (c *client) Close() (err error) {
 	defer func() {
 		// Always recover, a panic could be raised if c.quit was closed which
 		// means the Close method was called more than once.
@@ -100,7 +103,7 @@ func (c *Client) Close() (err error) {
 }
 
 // Send batch request.
-func (c *Client) send(msgs []interface{}) {
+func (c *client) send(msgs []interface{}) {
 	if len(msgs) == 0 {
 		return
 	}
@@ -119,12 +122,12 @@ func (c *Client) send(msgs []interface{}) {
 		if err := c.upload(b); err == nil {
 			break
 		}
-		Backo.Sleep(i)
+		time.Sleep(c.RetryAfter(i))
 	}
 }
 
 // Upload serialized batch message.
-func (c *Client) upload(b []byte) error {
+func (c *client) upload(b []byte) error {
 	url := c.Endpoint + "/v1/batch"
 	req, err := http.NewRequest("POST", url, bytes.NewReader(b))
 	if err != nil {
@@ -137,7 +140,7 @@ func (c *Client) upload(b []byte) error {
 	req.Header.Add("Content-Length", string(len(b)))
 	req.SetBasicAuth(c.key, "")
 
-	res, err := (&http.Client{Transport: c.Transport}).Do(req)
+	res, err := (&http.client{Transport: c.Transport}).Do(req)
 
 	if err != nil {
 		c.errorf("sending request - %s", err)
@@ -151,7 +154,7 @@ func (c *Client) upload(b []byte) error {
 }
 
 // Report on response body.
-func (c *Client) report(res *http.Response) {
+func (c *client) report(res *http.Response) {
 	if res.StatusCode < 400 {
 		c.debugf("response %s", res.Status)
 		return
@@ -167,7 +170,7 @@ func (c *Client) report(res *http.Response) {
 }
 
 // Batch loop.
-func (c *Client) loop() {
+func (c *client) loop() {
 	defer close(c.shutdown)
 
 	var msgs []interface{}
@@ -177,13 +180,15 @@ func (c *Client) loop() {
 	for {
 		select {
 		case msg := <-c.msgs:
-			c.debugf("buffer (%d/%d) %v", len(msgs), c.Size, msg)
+			c.debugf("buffer (%d/%d) %v", len(msgs), c.BatchSize, msg)
 			msgs = append(msgs, msg)
-			if len(msgs) == c.Size {
-				c.debugf("exceeded %d messages – flushing", c.Size)
+
+			if len(msgs) == c.BatchSize {
+				c.debugf("exceeded %d messages – flushing", c.BatchSize)
 				c.send(msgs)
 				msgs = nil
 			}
+
 		case <-tick.C:
 			if len(msgs) > 0 {
 				c.debugf("interval reached - flushing %d", len(msgs))
@@ -192,20 +197,15 @@ func (c *Client) loop() {
 			} else {
 				c.debugf("interval reached – nothing to send")
 			}
+
 		case <-c.quit:
 			c.debugf("exit requested – draining messages")
 
 			// Drain the msg channel, we have to close it first so no more
 			// messages can be pushed and otherwise the loop would never end.
-			//
-			// Note that this is will cause calls to the send methods to panic
-			// if the client is used after being closed, definitely not ideal,
-			// we should return an error like io.EOF instead. Since this has
-			// been the historical behavior already I'll assume it hasn't been
-			// a problem and I'll fix it later.
 			close(c.msgs)
 			for msg := range c.msgs {
-				c.debugf("buffer (%d/%d) %v", len(msgs), c.Size, msg)
+				c.debugf("buffer (%d/%d) %v", len(msgs), c.BatchSize, msg)
 				msgs = append(msgs, msg)
 			}
 
@@ -217,23 +217,16 @@ func (c *Client) loop() {
 	}
 }
 
-// Verbose log.
-func (c *Client) debugf(format string, args ...interface{}) {
+func (c *client) debugf(format string, args ...interface{}) {
 	if c.Verbose {
 		c.logf(format, args...)
 	}
 }
 
-// Unconditional log.
-func (c *Client) logf(format string, args ...interface{}) {
+func (c *client) logf(format string, args ...interface{}) {
 	c.Logger.Logf(format, args...)
 }
 
-func (c *Client) errorf(format string, args ...interface{}) {
+func (c *client) errorf(format string, args ...interface{}) {
 	c.Logger.Errorf(format, args...)
-}
-
-// Return uuid string.
-func uid() string {
-	return uuid.NewRandom().String()
 }
--- error.go
+++ error.go
@@ -2,6 +2,25 @@ package analytics
 
 import "fmt"
 
+// Returned by the `NewWithConfig` function when the one of the configuration
+// fields was set to an impossible value (like a negative duration).
+type ConfigError struct {
+
+	// A human-readable message explaining why the configuration field's value
+	// is invalid.
+	Reason string
+
+	// The name of the configuration field that was carrying an invalid value.
+	Field string
+
+	// The value of the configuration field that caused the error.
+	Value interface{}
+}
+
+func (e ConfigError) Error() string {
+	return fmt.Sprintf("analytics.NewWithConfig: %s (analytics.Config.%s: %#v)", e.Reason, e.Field, e.Value)
+}
+
 // Instances of this type are used to represent errors returned when a field was
 // no initialize properly in a structure passed as argument to one of the
 // functions of this package.
