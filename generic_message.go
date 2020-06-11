8aa2dea394f9ac19ee400ddab82a08fd5d29e01d
Jeremy Larkin <jeremy.larkin@segment.com>
2020-06-11 11:35:48 -0700

    add string() method to clean up type assertions

--- generic_message.go
+++ generic_message.go
@@ -10,41 +10,52 @@ func (msg GenericMessage) internal() {
 	panic(unimplementedError)
 }
 
+func (msg GenericMessage) string(field string) string {
+	val, _ := msg[field].(string)
+	return val
+}
+
 func (msg GenericMessage) Validate() error {
 	if typ, ok := msg["type"].(string); ok {
 		switch typ {
 		case "alias":
-			m := Alias{Type: "alias"}
-			m.UserId, _ = msg["userId"].(string)
-			m.PreviousId, _ = msg["previousId"].(string)
-			return m.Validate()
+			return Alias{
+				Type:       "alias",
+				UserId:     msg.string("userId"),
+				PreviousId: msg.string("previousId"),
+			}.Validate()
 		case "group":
-			m := Group{Type: "group"}
-			m.UserId, _ = msg["userId"].(string)
-			m.AnonymousId, _ = msg["anonymousId"].(string)
-			m.GroupId, _ = msg["groupId"].(string)
-			return m.Validate()
+			return Group{
+				Type:        "group",
+				UserId:      msg.string("userId"),
+				AnonymousId: msg.string("anonymousId"),
+				GroupId:     msg.string("groupId"),
+			}.Validate()
 		case "identify":
-			m := Identify{Type: "identify"}
-			m.UserId, _ = msg["userId"].(string)
-			m.AnonymousId, _ = msg["anonymousId"].(string)
-			return m.Validate()
+			return Identify{
+				Type:        "identify",
+				UserId:      msg.string("userId"),
+				AnonymousId: msg.string("anonymousId"),
+			}.Validate()
 		case "page":
-			m := Page{Type: "page"}
-			m.UserId, _ = msg["userId"].(string)
-			m.AnonymousId, _ = msg["anonymousId"].(string)
-			return m.Validate()
+			return Page{
+				Type:        "page",
+				UserId:      msg.string("userId"),
+				AnonymousId: msg.string("anonymousId"),
+			}.Validate()
 		case "screen":
-			m := Screen{Type: "screen"}
-			m.UserId, _ = msg["userId"].(string)
-			m.AnonymousId, _ = msg["anonymousId"].(string)
-			return m.Validate()
+			return Screen{
+				Type:        "screen",
+				UserId:      msg.string("userId"),
+				AnonymousId: msg.string("anonymousId"),
+			}.Validate()
 		case "track":
-			m := Track{Type: "track"}
-			m.UserId, _ = msg["userId"].(string)
-			m.AnonymousId, _ = msg["anonymousId"].(string)
-			m.Event, _ = msg["event"].(string)
-			return m.Validate()
+			return Track{
+				Type:        "track",
+				UserId:      msg.string("userId"),
+				AnonymousId: msg.string("anonymousId"),
+				Event:       msg.string("event"),
+			}.Validate()
 		}
 	}
 	return FieldError{
