89db81efba9ceb101169594be33cae259a45e3d4
Rohith BCS <rohith.bcs@gmail.com>
2021-01-19 23:02:27 +0530

    Added prefixes to custom headers

--- analytics.go
+++ analytics.go
@@ -428,9 +428,9 @@ func (c *client) upload(b []byte, targetNode string) error {
 	req.Header.Add("User-Agent", "analytics-go (version: "+Version+")")
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Content-Length", string(len(b)))
-	req.Header.Add("targetNode", targetNode)
-	req.Header.Add("nodeCount", strconv.Itoa(c.totalNodes))
-	req.Header.Add("userAgent", "serverSDK")
+	req.Header.Add("RS-targetNode", targetNode)
+	req.Header.Add("RS-nodeCount", strconv.Itoa(c.totalNodes))
+	req.Header.Add("RS-userAgent", "serverSDK")
 	req.SetBasicAuth(c.key, "")
 
 	res, err := c.http.Do(req)
